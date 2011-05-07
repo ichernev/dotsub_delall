ed9f9b644ddb99c5f3b746c06827ec8712a70d2a
Iskren Chernev <iskren.chernev@gmail.com>
2011-05-07 19:43:15 +0200

    Added GUI using jquery ui.

--- js/dom_injector.js
+++ js/dom_injector.js
@@ -17,23 +17,37 @@
 //
 (function() {
   if (document.getElementById("dotsub_delete_all")) {
+    // Code is injected, just send a message to initiate delete.
     var customEvent = document.createEvent('Event');
     customEvent.initEvent('deleteAllSubs', false, false);
     var communicationDiv = document.getElementById('dotsub_delete_all');
     communicationDiv.dispatchEvent(customEvent);
     return;
   }
+
+  var injectJS = function(path) {
+    var script;
+    script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = chrome.extension.getURL(path);
+    document.head.appendChild(script);
+  };
+
+  var injectCSS = function(path) {
+    var link;
+    link = document.createElement("link");
+    link.href = chrome.extension.getURL(path);
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    document.head.appendChild(link);
+  };
   
-  // Load jquery.
-  var script;
-  script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = chrome.extension.getURL("lib/jquery-1.6.js") + "?foo";
-  document.head.appendChild(script);
-  
-  // Load deleter script.
-  script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = chrome.extension.getURL("js/sub_deleter.js") + "?foo";
-  document.head.appendChild(script);
+  // jquery stuff
+  injectJS("lib/jquery-1.6.js");
+  injectJS("lib/jquery-ui-1.8.12.custom.min.js");
+  injectCSS("css/ui-lightness/jquery-ui-1.8.12.custom.css");
+
+  // my stuff
+  injectJS("js/sub_deleter.js");
+  injectCSS("css/sub_deleter.css");
 }());
--- js/sub_deleter.js
+++ js/sub_deleter.js
@@ -75,27 +75,103 @@
       var eta = (sub_ids.length - resp_recv) / delRate;
       console.log("req_send", req_send, "resp_recv", resp_recv, "total",
           sub_ids.length, "rate", delRate, "eta", eta);
-      if (resp_recv >= req_send && req_send === sub_ids.length) {
-        console.log("done");
-        window.clearInterval(window.reportInterval);
-      }
+      // if (resp_recv >= req_send && req_send === sub_ids.length) {
+      //   console.log("done");
+      //   window.clearInterval(window.reportInterval);
+      // }
     };
   
+    var finishDelete = function() {
+      console.log("done");
+      updateUI();
+      window.clearInterval(window.reportInterval);
+      $("#delall_dialog").remove();
+    };
+
     var sendReqs = function() {
       while (req_send - resp_recv < kMaxParallelRequests && req_send < sub_ids.length) {
         initiateDelete(sub_ids[req_send]);
       }
       if (resp_recv >= sub_ids.length) {
-        updateUI();
+        finishDelete();
       }
     };
   
-    var onDeleteRequest = function() {
-      console.log("delete request received");
+    var startDeleteProcess = function() {
       var sub_trs = $("table#captionTable tr").filter(function() {
         return this.id.slice(0, 7) === "caption";
       });
       sub_ids = sub_trs.map(function() { return this.id.slice(7); });
+
+      start_at = (new Date()).getTime();
+      req_send = resp_recv = 0;
+      idStatus = {};
+      sub_ids.each(function() { idStatus[this] = 0; });
+      sendReqs();
+      window.reportInterval = window.setInterval(uiReportProgress, 1000);
+    };
+
+    var uiTimes = 0;
+    var uiReportProgress = function() {
+      var elapsed = ((new Date()).getTime() - start_at) / 1000;
+      var delRate = resp_recv / (elapsed + 0.1);  // prevent division by zero
+      var eta = (sub_ids.length - resp_recv) / delRate;
+
+      var ui = $("#delall_dialog");
+      $(".send", ui).text(req_send);
+      $(".recv", ui).text(resp_recv);
+      $(".eta", ui).text(eta);
+
+      // ++ uiTimes;
+      // console.log("updated ui");
+      // if (uiTimes > 10) {
+      //   console.log("removing");
+      //   ui.remove();
+      //   window.clearInterval(window.uiReportInterval);
+      // }
+    };
+
+    var startFakeDeleteProcess = function() {
+      uiTimes = 0;
+      window.uiReportInterval = window.setInterval(uiReportProgress, 2000);
+    };
+
+    var showUI = function() {
+      // Clean up from previous dialog.
+      // TODO: Detect if delete is in progress.
+      $("#delall_dialog").remove();
+      
+      // Create new dialog.
+      var ui = $("<div id='delall_dialog'>" +
+          "<div class='btn'></div>" +
+          "<a class='crnt_subs'></a>" +
+          "</div>");
+      $(".btn", ui).button({ label: "Delete" }).click(function() {
+        $(this).parent().empty().append($("<div class='progress'>" +
+            "<span class='lbl'>send: </span><span class='send'>-</span>" +
+            "<span class='lbl'>recv: </span><span class='recv'>-</span>" +
+            "<span class='lbl'>ETA: </span><span class='eta'>-</span>" +
+            "</div>"));
+        startDeleteProcess();
+      });
+      $(".crnt_subs", ui).
+        attr("href",
+          $('div#advancedMenu > ul > li > a[href$="srt"]').attr("href")).
+        text("download current subtitles");
+      ui.
+        appendTo($("body")).
+        dialog({
+          title: "Delete all subtitles?",
+          modal: true,
+          resizable: false,
+          draggable: false
+      });
+    };
+
+    var onDeleteRequest = function() {
+      console.log("delete request received");
+      showUI();
+
       // console.log("got " + sub_trs.length + " subtitles");
       // for (var i = 0, fin = 5 < sub_trs.length ? 5 : sub_trs.length; i < fin; ++i) {
       //   console.log("sample subtitle id " + sub_trs[i].id);
@@ -104,13 +180,7 @@
       // sub_trs.each(function() {
       //     initiateDelete(this.id.slice(7));
       // });
-      start_at = (new Date()).getTime();
-      req_send = resp_recv = 0;
-      idStatus = {};
-      sub_ids.each(function() { idStatus[this] = 0; });
-      sendReqs();
   
-      window.reportInterval = window.setInterval(reportProgress, 1000);
       // if (sub_trs.length >= 1) {
       //   var sub_id = sub_trs[0].id.slice(7);
       //   console.info("initiating delete of sub id " + sub_id);
@@ -135,11 +205,11 @@
 
   var interval;
   interval = window.setInterval(function() {
-    if (jQuery !== undefined) {
+    if (window.jQuery !== undefined && window.jQuery.fn.button !== undefined) {
       closure(jQuery);
       window.clearInterval(interval);
     } else {
-      console.log('jquery not loaded');
+      console.log('[deleter] jquery|ui not loaded');
     }
   }, 500);
 }());
